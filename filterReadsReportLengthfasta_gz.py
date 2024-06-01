6474f0a2f11046519abb4f44b5cbe7f2c3e0e657
Xuewen Wang <xwwang@uga.edu>
2024-05-31 23:55:33 -0500

    Update filterReadsReportLengthfasta_gz.py

--- filterReadsReportLengthfasta_gz.py
+++ filterReadsReportLengthfasta_gz.py
@@ -8,18 +8,18 @@ from Bio import SeqIO
 FUNCTION:
 This script will take the next generation sequencing reads, e.g. PacBio SMART sequencing reads, from the input file in fasta format
 Then will filter the sequence based on the cutoff value of minimal sequence length in bp. The reads longer than this cutoff will be output to the result file.
-Parameters: outfileName in_seqfile.fastq.gz length_cutoff
+Parameters: outfileName in_seqfile.fasta.gz length_cutoff
 USAGE: 
 Example on testing data:
-`python3 filterReadsReportLengthfastq_gz ./testseq.lengths.txt ./testseq.fastq 1000` 
+`python3 filterReadsReportLengthfasta_gz ./testseq.lengths.txt ./testseq.fasta 1000` 
 Path information could be added before the input and output file name if files are not in current directroy.
 Version 2024-April 1st
 # use python version 3
 '''
 __author__ ="Xuewen Wang"
 
-usage=""" python3 filterReadsReportLengthfasta_gz.py OUT_seqId_length.txt IN_seq_file_in_fastq.gz length_cutoff
-          e.g.: python3 filterReadsReportLengthfastq_gz.py ./testseq.lengths.txt ./testseq.fastq.gz 1000 
+usage=""" python3 filterReadsReportLengthfasta_gz.py OUT_seqId_length.txt IN_seq_file_in_fasta.gz length_cutoff
+          e.g.: python3 filterReadsReportLengthfasta_gz.py ./testseq.lengths.txt ./testseq.fasta.gz 1000 
           """
 #print(usage)
 
